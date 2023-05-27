263f688bc9d91954702b68132ff65ec384759b63
Vladas <146100@gmail.com>
2023-05-27 09:46:01 +0300

    lock counters

--- refs/src/ref_counters.rs
+++ refs/src/ref_counters.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    sync::Mutex,
+};
 
 /// Read the name of the type
 type CounterAndDeallocator = (usize, Box<dyn FnOnce()>);
@@ -26,12 +29,16 @@ pub(crate) struct RefCounters {
 }
 static_default!(RefCounters);
 
+static LOCK: Mutex<()> = Mutex::new(());
+
 impl RefCounters {
     pub(crate) fn exists(addr: usize) -> bool {
+        let _lock = LOCK.lock().unwrap();
         Self::get().counters.contains_key(&addr)
     }
 
     pub(crate) fn strong_count(addr: usize) -> usize {
+        let _lock = LOCK.lock().unwrap();
         match Self::get().counters.get(&addr) {
             Some(counter) => counter.0,
             None => 0,
@@ -39,22 +46,26 @@ impl RefCounters {
     }
 
     pub(crate) fn add_strong(addr: usize, dealloc_fn: impl FnOnce() + 'static) {
-        if Self::exists(addr) {
-            Self::increase_strong(addr)
+        let _lock = LOCK.lock().unwrap();
+        if let Entry::Vacant(entry) = Self::get().counters.entry(addr) {
+            entry.insert((1, Box::new(dealloc_fn)));
         } else {
-            Self::get().counters.insert(addr, (1, Box::new(dealloc_fn)));
+            Self::get().counters.get_mut(&addr).expect("Failed to increase strong count").0 += 1;
         }
     }
 
     pub(crate) fn increase_strong(addr: usize) {
+        let _lock = LOCK.lock().unwrap();
         Self::get().counters.get_mut(&addr).expect("Failed to increase strong count").0 += 1;
     }
 
     pub(crate) fn decrease_strong(addr: usize) {
+        let _lock = LOCK.lock().unwrap();
         Self::get().counters.get_mut(&addr).expect("Failed to decrease strong count").0 -= 1;
     }
 
     pub(crate) fn remove(addr: usize) {
+        let _lock = LOCK.lock().unwrap();
         let counter = Self::get().counters.remove(&addr).expect("Removing non existing address");
 
         // Call dealloc
