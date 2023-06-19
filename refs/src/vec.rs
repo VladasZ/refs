396cedf89ade664c6d75cc6368c4a8c0d3414b00
Vladas <146100@gmail.com>
2023-06-19 22:52:37 +0300

    to_own_vec

--- refs/src/vec.rs
+++ refs/src/vec.rs
@@ -3,11 +3,11 @@ use crate::{Own, ToOwn};
 pub type OwnVec<T> = Vec<Own<T>>;
 
 pub trait ToOwnVec<T> {
-    fn to_own(self) -> OwnVec<T>;
+    fn to_own_vec(self) -> OwnVec<T>;
 }
 
 impl<T: 'static> ToOwnVec<T> for Vec<T> {
-    fn to_own(self) -> OwnVec<T> {
+    fn to_own_vec(self) -> OwnVec<T> {
         self.into_iter().map(ToOwn::to_own).collect()
     }
 }
@@ -26,7 +26,7 @@ mod test {
     fn convert() {
         set_current_thread_as_main();
         let vec: Vec<u32> = vec![1, 2, 3, 4, 5];
-        let owned_vec: OwnVec<u32> = vec.to_own();
+        let owned_vec: OwnVec<u32> = vec.to_own_vec();
 
         dbg!(owned_vec);
     }
@@ -36,8 +36,8 @@ mod test {
     fn eq() {
         set_current_thread_as_main();
         let vec: Vec<u32> = vec![1, 2, 3, 4, 5];
-        let owned_vec: OwnVec<u32> = vec.clone().to_own();
-        let owned_vec2: OwnVec<u32> = vec.clone().to_own();
+        let owned_vec: OwnVec<u32> = vec.clone().to_own_vec();
+        let owned_vec2: OwnVec<u32> = vec.clone().to_own_vec();
 
         assert_eq!(owned_vec, owned_vec2);
         assert_eq!(owned_vec, vec);
